2cf8534e245b0d996ac15a7b6f38702f7ad7cd14
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-02-20 13:28:03 -0600

    Test subsetting.

--- tests/testthat/test-setgendata.r
+++ tests/testthat/test-setgendata.r
@@ -16,6 +16,9 @@ nRows <- 3
 nCols <- 3
 genotypes <- simPED(filename = pedPath, n = nRows, p = nCols, returnGenos = TRUE)
 
+
+context("setGenData")
+
 test_that("setGenData complains if folderOut already exists", {
     dirExistsPath <- paste0(tmpPath, "dirExists")
     dir.create(dirExistsPath, showWarnings = FALSE)
@@ -23,4 +26,95 @@ test_that("setGenData complains if folderOut already exists", {
         setGenData(fileIn = pedPath, dataType = 'integer', header = TRUE,
                    n = nRows, folderOut = dirExistsPath)
     )
+})
+
+
+#####
+context("rDMatrix")
+
+rGenData <- setGenData(fileIn = pedPath, dataType = 'integer',
+                      header = TRUE, n = nRows,
+                      folderOut = paste0(tmpPath, randomString()))
+
+test_that("subsetting", {
+
+    expect_equal(all.equal(rGenData@geno[], genotypes), TRUE)
+
+    # expect_equal(all.equal(rGenData@geno[1], genotypes[1]), TRUE) Not implemented yet
+    expect_equal(all.equal(rGenData@geno[1, ], genotypes[1, ]), TRUE)
+    expect_equal(all.equal(rGenData@geno[, 1], genotypes[, 1]), TRUE)
+    expect_equal(all.equal(rGenData@geno[1, 1], genotypes[1, 1]), TRUE)
+    expect_equal(all.equal(rGenData@geno[1, , drop = FALSE], genotypes[1, , drop = FALSE]), TRUE)
+    expect_equal(all.equal(rGenData@geno[, 1, drop = FALSE], genotypes[, 1, drop = FALSE]), TRUE)
+    expect_equal(all.equal(rGenData@geno[1, 1, drop = FALSE], genotypes[1, 1, drop = FALSE]), TRUE)
+
+    # expect_equal(all.equal(rGenData@geno[1:2], genotypes[1:2]), TRUE)  Not implemented yet
+    expect_equal(all.equal(rGenData@geno[1:2, ], genotypes[1:2, ]), TRUE)
+    expect_equal(all.equal(rGenData@geno[, 1:2], genotypes[, 1:2]), TRUE)
+    expect_equal(all.equal(rGenData@geno[1:2, 1:2], genotypes[1:2, 1:2]), TRUE)
+    expect_equal(all.equal(rGenData@geno[1:2, , drop = FALSE], genotypes[1:2, , drop = FALSE]), TRUE)
+    expect_equal(all.equal(rGenData@geno[, 1:2, drop = FALSE], genotypes[, 1:2, drop = FALSE]), TRUE)
+    expect_equal(all.equal(rGenData@geno[1:2, 1:2, drop = FALSE], genotypes[1:2, 1:2, drop = FALSE]), TRUE)
+
+    # expect_equal(all.equal(rGenData@geno[2:1], genotypes[2:1]), TRUE) Not implemented yet
+    expect_equal(all.equal(rGenData@geno[2:1, ], genotypes[2:1, ]), TRUE)
+    expect_equal(all.equal(rGenData@geno[, 2:1], genotypes[, 2:1]), TRUE)
+    expect_equal(all.equal(rGenData@geno[2:1, 2:1], genotypes[2:1, 2:1]), TRUE)
+    expect_equal(all.equal(rGenData@geno[2:1, , drop = FALSE], genotypes[2:1, , drop = FALSE]), TRUE)
+    expect_equal(all.equal(rGenData@geno[, 2:1, drop = FALSE], genotypes[, 2:1, drop = FALSE]), TRUE)
+    expect_equal(all.equal(rGenData@geno[2:1, 2:1, drop = FALSE], genotypes[2:1, 2:1, drop = FALSE]), TRUE)
+
+    # expect_equal(all.equal(rGenData@geno[c(3, 1)], genotypes[c(3, 1)]), TRUE) Not implemented yet
+    expect_equal(all.equal(rGenData@geno[c(3, 1), ], genotypes[c(3, 1), ]), TRUE)
+    expect_equal(all.equal(rGenData@geno[, c(3, 1)], genotypes[, c(3, 1)]), TRUE)
+    expect_equal(all.equal(rGenData@geno[c(3, 1), c(3, 1)], genotypes[c(3, 1), c(3, 1)]), TRUE)
+    expect_equal(all.equal(rGenData@geno[c(3, 1), , drop = FALSE], genotypes[c(3, 1), , drop = FALSE]), TRUE)
+    expect_equal(all.equal(rGenData@geno[, c(3, 1), drop = FALSE], genotypes[, c(3, 1), drop = FALSE]), TRUE)
+    expect_equal(all.equal(rGenData@geno[c(3, 1), c(3, 1), drop = FALSE], genotypes[c(3, 1), c(3, 1), drop = FALSE]), TRUE)
+
+})
+
+
+#####
+context("cDMatrix")
+
+cGenData <- setGenData(fileIn = pedPath, dataType = 'integer',
+                       header = TRUE, n = nRows, distributed.by = "columns",
+                       folderOut = paste0(tmpPath, randomString()))
+
+test_that("subsetting", {
+
+    expect_equal(all.equal(cGenData@geno[], genotypes), TRUE)
+
+    # expect_equal(all.equal(cGenData@geno[1], genotypes[1]), TRUE) Not implemented yet
+    expect_equal(all.equal(cGenData@geno[1, ], genotypes[1, ]), TRUE)
+    expect_equal(all.equal(cGenData@geno[, 1], genotypes[, 1]), TRUE)
+    expect_equal(all.equal(cGenData@geno[1, 1], genotypes[1, 1]), TRUE)
+    expect_equal(all.equal(cGenData@geno[1, , drop = FALSE], genotypes[1, , drop = FALSE]), TRUE)
+    expect_equal(all.equal(cGenData@geno[, 1, drop = FALSE], genotypes[, 1, drop = FALSE]), TRUE)
+    expect_equal(all.equal(cGenData@geno[1, 1, drop = FALSE], genotypes[1, 1, drop = FALSE]), TRUE)
+
+    # expect_equal(all.equal(cGenData@geno[1:2], genotypes[1:2]), TRUE)  Not implemented yet
+    expect_equal(all.equal(cGenData@geno[1:2, ], genotypes[1:2, ]), TRUE)
+    expect_equal(all.equal(cGenData@geno[, 1:2], genotypes[, 1:2]), TRUE)
+    expect_equal(all.equal(cGenData@geno[1:2, 1:2], genotypes[1:2, 1:2]), TRUE)
+    expect_equal(all.equal(cGenData@geno[1:2, , drop = FALSE], genotypes[1:2, , drop = FALSE]), TRUE)
+    expect_equal(all.equal(cGenData@geno[, 1:2, drop = FALSE], genotypes[, 1:2, drop = FALSE]), TRUE)
+    expect_equal(all.equal(cGenData@geno[1:2, 1:2, drop = FALSE], genotypes[1:2, 1:2, drop = FALSE]), TRUE)
+
+    # expect_equal(all.equal(cGenData@geno[2:1], genotypes[2:1]), TRUE) Not implemented yet
+    expect_equal(all.equal(cGenData@geno[2:1, ], genotypes[2:1, ]), TRUE)
+    expect_equal(all.equal(cGenData@geno[, 2:1], genotypes[, 2:1]), TRUE)
+    expect_equal(all.equal(cGenData@geno[2:1, 2:1], genotypes[2:1, 2:1]), TRUE)
+    expect_equal(all.equal(cGenData@geno[2:1, , drop = FALSE], genotypes[2:1, , drop = FALSE]), TRUE)
+    expect_equal(all.equal(cGenData@geno[, 2:1, drop = FALSE], genotypes[, 2:1, drop = FALSE]), TRUE)
+    expect_equal(all.equal(cGenData@geno[2:1, 2:1, drop = FALSE], genotypes[2:1, 2:1, drop = FALSE]), TRUE)
+
+    # expect_equal(all.equal(cGenData@geno[c(3, 1)], genotypes[c(3, 1)]), TRUE) Not implemented yet
+    expect_equal(all.equal(cGenData@geno[c(3, 1), ], genotypes[c(3, 1), ]), TRUE)
+    expect_equal(all.equal(cGenData@geno[, c(3, 1)], genotypes[, c(3, 1)]), TRUE)
+    expect_equal(all.equal(cGenData@geno[c(3, 1), c(3, 1)], genotypes[c(3, 1), c(3, 1)]), TRUE)
+    expect_equal(all.equal(cGenData@geno[c(3, 1), , drop = FALSE], genotypes[c(3, 1), , drop = FALSE]), TRUE)
+    expect_equal(all.equal(cGenData@geno[, c(3, 1), drop = FALSE], genotypes[, c(3, 1), drop = FALSE]), TRUE)
+    expect_equal(all.equal(cGenData@geno[c(3, 1), c(3, 1), drop = FALSE], genotypes[c(3, 1), c(3, 1), drop = FALSE]), TRUE)
 })
