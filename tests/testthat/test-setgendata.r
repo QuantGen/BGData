df3cbc53bca885b5fdb77d50efec69f061bc3c76
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-02-20 11:34:17 -0600

    Test if setGenData complains if folderOut already exists.

--- tests/testthat/test-setgendata.r
+++ tests/testthat/test-setgendata.r
@@ -15,3 +15,12 @@ pedPath <- paste0(tmpPath, 'ped-', randomString(), '.txt')
 nRows <- 3
 nCols <- 3
 genotypes <- simPED(filename = pedPath, n = nRows, p = nCols, returnGenos = TRUE)
+
+test_that("setGenData complains if folderOut already exists", {
+    dirExistsPath <- paste0(tmpPath, "dirExists")
+    dir.create(dirExistsPath, showWarnings = FALSE)
+    expect_error(
+        setGenData(fileIn = pedPath, dataType = 'integer', header = TRUE,
+                   n = nRows, folderOut = dirExistsPath)
+    )
+})
