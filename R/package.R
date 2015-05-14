1e6ea1f73d46bf718a50bff5397d6b24a628f35c
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-05-14 19:20:02 +0200

    Fix \link and \url mix-up.

--- R/package.R
+++ R/package.R
@@ -1,6 +1,6 @@
 #' BGData: Memory Mapped Matrices and Data-Structures for Genomic Data for R
 #'
-#' More info in the wiki: \link{https://github.com/QuantGen/BGData/wiki}
+#' More info in the wiki: \url{https://github.com/QuantGen/BGData/wiki}
 #'
 #' @docType package
 #' @name BGData
