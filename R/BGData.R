a4da386ee0f2bcbc9448ae029f842f048de9d3a7
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-05-14 19:09:21 +0200

    Remove duplicated \code tags.

--- R/BGData.R
+++ R/BGData.R
@@ -48,32 +48,31 @@ setMethod('initialize','BGData',function(.Object,geno,pheno,map){
 #' \code{readPED} assumes that the plaintext file (\code{fileIn}) contains 
 #' records of individuals in rows, and phenotypes, covariates and markers in 
 #' columns. The columns included in columns \code{1:nColSkip} are used to 
-#' populate the slot \code{\code{@@pheno}} of a \code{\linkS4class{BGData}} 
-#' object, and the remaining columns are used to fill the slot 
-#' \code{\code{@@geno}}. If the first row contains a header 
-#' (\code{header=TRUE}), data in this row is used to determine variables names 
-#' for \code{@@pheno} and marker names for \code{@@map} and \code{@@geno}. 
-#' Genotypes are stored in a distributed matrix (\code{mmMatrix}). By default a 
-#' column-distributed (\code{\linkS4class{cmmMatrix}}) is used for 
-#' \code{@@geno}, but the user can modify this using the \code{distributed.by} 
-#' argument. The number of chunks is either specified by the user (use 
-#' \code{nChunks} when calling \code{readPED}) or determined internally so that 
-#' each \code{ff_matrix} object has a number of cells that is smaller than 
-#' \code{.Machine$integer.max/1.2}. \code{readPED} creates a folder 
-#' (\code{folderOut}) that contains the binary flat files (\code{geno_*.bin}) 
-#' and the \code{\linkS4class{BGData}} object (typically named 
-#' \code{BGData.RData}. Optionally (if \code{returnData} is TRUE) it returns the
-#' \code{\linkS4class{BGData}} object to the environment. The filename of the 
-#' \code{ff_matrix} objects are saved as relative names. Therefore, to be able 
-#' to access the content of the data included in \code{@@geno} the working 
-#' directory must either be the folder where these files are saved 
-#' (\code{folderOut}) or the object must be loaded either using 
+#' populate the slot \code{@@pheno} of a \code{\linkS4class{BGData}} object, and
+#' the remaining columns are used to fill the slot \code{@@geno}. If the first
+#' row contains a header (\code{header=TRUE}), data in this row is used to
+#' determine variables names for \code{@@pheno} and marker names for
+#' \code{@@map} and \code{@@geno}. Genotypes are stored in a distributed matrix
+#' (\code{mmMatrix}). By default a column-distributed
+#' (\code{\linkS4class{cmmMatrix}}) is used for \code{@@geno}, but the user can
+#' modify this using the \code{distributed.by} argument. The number of chunks is
+#' either specified by the user (use \code{nChunks} when calling \code{readPED})
+#' or determined internally so that each \code{ff_matrix} object has a number of
+#' cells that is smaller than \code{.Machine$integer.max/1.2}. \code{readPED}
+#' creates a folder (\code{folderOut}) that contains the binary flat files
+#' (\code{geno_*.bin}) and the \code{\linkS4class{BGData}} object (typically
+#' named \code{BGData.RData}. Optionally (if \code{returnData} is TRUE) it
+#' returns the \code{\linkS4class{BGData}} object to the environment. The
+#' filename of the \code{ff_matrix} objects are saved as relative names.
+#' Therefore, to be able to access the content of the data included in
+#' \code{@@geno} the working directory must either be the folder where these
+#' files are saved (\code{folderOut}) or the object must be loaded either using 
 #' \code{loadBGData} or \code{load2}.
 #' 
 #' @param fileIn The path to the plaintext file.
 #' @param header If TRUE, the file contains a header.
-#' @param dataType The coding of genotypes. Use \code{integer()} or
-#'   \code{double()} for numeric coding. Character coding is currently not
+#' @param dataType The coding of genotypes. Use \code{integer()} or 
+#'   \code{double()} for numeric coding. Character coding is currently not 
 #'   supported: use the \code{--recodeA} option of PLINK to convert the PED file
 #'   into a raw file.
 #' @param n The number of individuals.
@@ -131,15 +130,15 @@ readPED<-function(fileIn,header,dataType,n=NULL,p=NULL,na.strings='NA',
 #' \code{readPED.matrix} assumes that the plaintext file (\code{fileIn}) 
 #' contains records of individuals in rows, and phenotypes, covariates and 
 #' markers in columns. The columns included in columns \code{1:nColSkip} are 
-#' used to populate the slot \code{\code{@@pheno}} of a 
-#' \code{\linkS4class{BGData}} object, and the remaining columns are used to 
-#' fill the slot \code{\code{@@geno}}. If the first row contains a header 
-#' (\code{header=TRUE}), data in this row is used to determine variables names 
-#' for \code{@@pheno} and marker names for \code{@@map} and \code{@@geno}.
+#' used to populate the slot \code{@@pheno} of a \code{\linkS4class{BGData}}
+#' object, and the remaining columns are used to fill the slot \code{@@geno}. If
+#' the first row contains a header (\code{header=TRUE}), data in this row is
+#' used to determine variables names for \code{@@pheno} and marker names for
+#' \code{@@map} and \code{@@geno}.
 #' 
 #' @param fileIn The path to the plaintext file.
 #' @param header If TRUE, the file contains a header.
-#' @param dataType The coding of genotypes. Use \code{character()} for A/C/G/T
+#' @param dataType The coding of genotypes. Use \code{character()} for A/C/G/T 
 #'   or \code{integer()} for numeric coding.
 #' @param n The number of individuals.
 #' @param p The number of markers.
