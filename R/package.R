035f4189a8222fe480539037f2b4c90acf4f756f
Alexander Grüneberg <alexander.grueneberg@googlemail.com>
2015-05-14 18:47:09 +0200

    Add package documentation.
    To access: `package?BGData`

---
+++ R/package.R
@@ -0,0 +1,7 @@
+#' BGData: Memory Mapped Matrices and Data-Structures for Genomic Data for R
+#'
+#' More info in the wiki: \link{https://github.com/QuantGen/BGData/wiki}
+#'
+#' @docType package
+#' @name BGData
+NULL
