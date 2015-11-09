939c97e1eeab0661c4c19acccffa9bef774e532e
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2015-11-09 16:42:24 -0500

    Suppress and check for warning in normalizeType.

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -204,7 +204,8 @@ test_that("normalizeType", {
     expect_equal(typeof(normalizeType(character())), "character")
     expect_equal(typeof(normalizeType("complex")), "complex")
     expect_equal(typeof(normalizeType(complex())), "complex")
-    expect_equal(typeof(normalizeType("test")), "character")
+    expect_warning(typeof(normalizeType("test")), "character")
+    expect_equal(suppressWarnings(typeof(normalizeType("test"))), "character")
     expect_equal(typeof(normalizeType(1)), "double")
     expect_equal(typeof(normalizeType(1L)), "integer")
 
