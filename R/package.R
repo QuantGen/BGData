936fe3fb2550df677ae8b5bb278da499b68a6dac
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2016-02-03 15:26:56 -0500

    Remove startup message for now.

--- R/package.R
+++ R/package.R
@@ -9,9 +9,3 @@
 #' @importMethodsFrom symDMatrix diag
 #' @aliases NULL
 NULL
-
-
-.onAttach <- function(libname, pkgname) {
-    packageStartupMessage("The BGData package was supported by the National Institutes of Health (Grant: R01GM101219, R01GM099992).")
-    packageStartupMessage()
-}
