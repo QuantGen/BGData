0527f74f837da5158b0784a75bc952d3e324fa77
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2017-05-09 06:24:08 -0400

    Disable multicore on WinBuilder

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -7,8 +7,13 @@ testPath <- paste0("/tmp/BGData-", BGData:::randomString(), "/")
 dir.create(testPath)
 
 hasCores <- function(numCores) {
+    # For CRAN
     if (Sys.getenv("_R_CHECK_LIMIT_CORES_") == TRUE || numCores > parallel::detectCores()) {
         skip("Not enough cores or number of cores capped for CRAN submission checks.")
+    }
+    # For WinBuilder
+    if (.Platform$OS.type == "windows" && numCores > 1) {
+        skip("mc.cores > 1 is not supported on Windows.")
     }
 }
 
