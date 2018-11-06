2ceee7a13f162b3a6ab718b30d000e669e345c66
Alexander Grueneberg <alexander.grueneberg@googlemail.com>
2018-11-06 11:39:50 -0500

    tests: Test summarize on integer data

--- tests/testthat/test-summarize.R
+++ tests/testthat/test-summarize.R
@@ -1,37 +1,50 @@
 context("summarize")
 
-for (nCores in seq_len(2)) {
+set.seed(1)
 
-    test_that(paste("summarize", "on", nCores, "cores"), {
+n <- 5
+p <- 10
 
-        hasCores(nCores)
-
-        genotypes <- matrix(data = c(0, 0, 1, 0, 2, 2, 1, 2, 0, 1, 2, 0, 0, 1, 2, 0, NA, 0), nrow = 3, ncol = 6)
-
-        computeDummy <- function(i = seq_len(nrow(genotypes)), j = seq_len(ncol(genotypes))) {
-            dummy <- data.frame(
-                freq_na = vector(mode = "double", length = length(j)),
-                allele_freq = vector(mode = "double", length = length(j)),
-                sd = vector(mode = "double", length = length(j))
-            )
-            for (col in seq_along(j)) {
-                Z <- genotypes[i, j[col]]
-                NAs <- sum(is.na(Z))
-                dummy$freq_na[col] <- NAs / length(Z)
-                dummy$allele_freq[col] <- sum(Z, na.rm = TRUE) / ((length(Z) - NAs) * 2)
-                dummy$sd[col] <- sd(Z, na.rm = TRUE)
-            }
-            return(dummy)
-        }
-
-        for (chunkSize in c(3, 6)) {
-            expect_equal(summarize(X = genotypes, chunkSize = chunkSize, nCores = nCores), computeDummy())
-            expect_equal(summarize(X = genotypes, i = c(1, 3), chunkSize = chunkSize, nCores = nCores), computeDummy(i = c(1, 3)))
-            expect_equal(summarize(X = genotypes, j = c(1, 3, 5), chunkSize = chunkSize, nCores = nCores), computeDummy(j =  c(1, 3, 5)))
-            expect_equal(summarize(X = genotypes, i = c(1, 3), j = c(1, 3, 5), chunkSize = chunkSize, nCores = nCores), computeDummy(i = c(1, 3), j = c(1, 3, 5)))
-            expect_equal(summarize(X = genotypes, nCores = nCores), computeDummy())
-        }
-
-    })
-
+summarize_R <- function(X, i = seq_len(nrow(X)), j = seq_len(ncol(X))) {
+    res <- data.frame(
+        freq_na = vector(mode = "double", length = length(j)),
+        allele_freq = vector(mode = "double", length = length(j)),
+        sd = vector(mode = "double", length = length(j))
+    )
+    for (col in seq_along(j)) {
+        x <- X[i, j[col]]
+        nMissing <- sum(is.na(x))
+        res$freq_na[col] <- nMissing / length(x)
+        res$allele_freq[col] <- sum(x, na.rm = TRUE) / ((length(x) - nMissing) * 2)
+        res$sd[col] <- sd(x, na.rm = TRUE)
+    }
+    return(res)
 }
+
+test_that("summarize", {
+
+    for (mode in c("integer", "double")) {
+
+        X <- matrix(data = rnorm(n * p, sd = 100), nrow = n, ncol = p)
+        X[sample(1:length(X), size = 5)] <- NA
+        storage.mode(X) <- mode
+
+        for (nCores in seq_len(2)) {
+
+            hasCores(nCores)
+
+            for (chunkSize in c(3, 6)) {
+
+                expect_equal(summarize(X = X, chunkSize = chunkSize, nCores = nCores), summarize_R(X))
+                expect_equal(summarize(X = X, i = c(1, 3), chunkSize = chunkSize, nCores = nCores), summarize_R(X, i = c(1, 3)))
+                expect_equal(summarize(X = X, j = c(1, 3, 5), chunkSize = chunkSize, nCores = nCores), summarize_R(X, j =  c(1, 3, 5)))
+                expect_equal(summarize(X = X, i = c(1, 3), j = c(1, 3, 5), chunkSize = chunkSize, nCores = nCores), summarize_R(X, i = c(1, 3), j = c(1, 3, 5)))
+                expect_equal(summarize(X = X, nCores = nCores), summarize_R(X))
+
+            }
+
+        }
+
+    }
+
+})
