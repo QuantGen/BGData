945a83cc5f966e4f682aa606fc7fb8869bce3296
Alexander Grueneberg <git@agrueneberg.info>
2020-05-12 17:18:25 -0400

    preprocess: Fix typo (vector -> matrix)

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,6 +1,6 @@
 preprocess <- function(X, center = FALSE, scale = FALSE, impute = FALSE) {
     if (!(is.numeric(X) && length(dim(X)) == 2)) {
-        stop("'X' needs to be a numeric vector")
+        stop("'X' needs to be a numeric matrix")
     }
     if (!(is.logical(center) && length(center) == 1L) && !(is.numeric(center) && length(center) == ncol(X))) {
         stop("'center' needs to be either a logical vector of size 1 or a numeric vector of size 'ncol(X)'")
